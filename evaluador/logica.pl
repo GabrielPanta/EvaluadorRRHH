6db09a76e6994756d878937cb6d416d13417d650
gabrielpanta <gabrielpantajimenez@hotmail.com>
2025-07-09 19:37:48 -0500

    se agregó botones para dirigir a vistas de puestos

--- evaluador/logica.pl
+++ evaluador/logica.pl
@@ -1,4 +1,4 @@
-% Generado automï¿½ticamente
+% Generado automáticamente
 
 experiencia(clara, 3).
 habilidad(clara, programacion).
@@ -9,6 +9,16 @@ habilidad(gabriel, trabajo_en_equipo).
 experiencia(eduardo, 3).
 habilidad(eduardo, programacion).
 habilidad(eduardo, trabajo_en_equipo).
+experiencia(ruth, 1).
+habilidad(ruth, liderazgo).
+experiencia(carlos, 2).
+habilidad(carlos, liderazgo).
+experiencia(ana, 3).
+habilidad(ana, programacion).
+habilidad(ana, trabajo_en_equipo).
+experiencia(gabriel_panta_jimenez, 5).
+habilidad(gabriel_panta_jimenez, programacion).
+habilidad(gabriel_panta_jimenez, trabajo_en_equipo).
 
 
 elegible(Nombre) :-
